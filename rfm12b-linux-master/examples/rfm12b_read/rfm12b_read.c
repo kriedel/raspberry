108508e7170a768db5b1180b7ffcd0fb8e152af3
Kai <kairiedel@yahoo.de>
2013-11-05 07:55:45 +0100

    Update HTML sites
    Highchart Graph with one day timescale

--- rfm12b-linux-master/examples/rfm12b_read/rfm12b_read.c
+++ rfm12b-linux-master/examples/rfm12b_read/rfm12b_read.c
@@ -1,59 +1,36 @@
 /* rfm12b-linux: linux kernel driver for the rfm12(b) RF module by HopeRF
 *  Copyright (C) 2013 Georg Kaindl
-*  
-*  This file is part of rfm12b-linux.
-*  
-*  rfm12b-linux is free software: you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation, either version 2 of the License, or
-*  (at your option) any later version.
-*  
-*  rfm12b-linux is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*  
-*  You should have received a copy of the GNU General Public License
-*  along with rfm12b-linux.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 /*
-   DOCUMENTATION:
-   
-   This is a trivial example that will open the rfm12b device and
-   continously read() from it in an endless loop. The individual
-   bytes of the received packet will then be written to stdout.
-   
-   On the receiving side, the driver will block on read() until a
-   packet is received. A subsequent read will then return the entire
-   packet, provided your userspace buffer is large enough (if your
-   buffer is too small, the rest of the bytes in the packet will be
-   lost forever). Since the maximum packet length is 66 bytes (to
-   ensure compatibility with the JeeNode Arduino library), you should
-   provide a buffer at least of this size. The return value of read()
-   behaves as expected, and if it's a positive number, it's the length
-   of the packet that has been received.
-   
-   Press Ctrl+C (e.g. send a SIGINT) to quit the program. It will
-   then print the driver statistics.
-   
-   You can use this example as a receiver to the rfm12b_write.c example
-   or as receiver for the rfm12b_send Arduino example. It's also useful
-   to test or debug your own sending code.
-*/
+ ============================================================================
+ Name        : url.c
+ Author      : Kai Riedel
+ Version     : 1.0
+ Copyright   :
+ Description : receive program for RFM12 modules
+ ============================================================================
+ */
+
+//first install libcurl package
+//sudo apt-get install libcurl4-openssl-dev
+//compile with: use make in ../ directory
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <string.h>
 #include <time.h>
 #include <signal.h>
 #include <errno.h>
+#include <curl/curl.h>
 
 #include "../common/common.h"
 #include "../../rfm12b_config.h"
 #include "../../rfm12b_ioctl.h"
 
 #define RF12_BUF_LEN   128
+
+float temperature;
+char humidity;
 
 static volatile int running;
 
@@ -63,14 +40,40 @@ void sig_handler(int signum)
    running = 0;
 }
 
+size_t write_data(void *ptr, size_t size, size_t count, void *stream)
+{
+ 
+  char * pch;
+  pch = strstr (ptr,"Temperature");
+  strncpy ( pch, &pch[12], 5 );
+  //puts (tch);
+  temperature = atof(pch);
+  //printf ("%.1f\n", temperature); 
+  pch = strstr (ptr,"Humidity");
+  strncpy ( pch, &pch[10], 2 ); 
+  //puts (pch);
+  humidity = atoi(pch); 
+  //printf ("%d\n", humidity);
+  
+  //sprintf(pch, "%s", ptr);
+}
+
 int main(int argc, char** argv)
 {
+   CURL *curl;
+   CURLcode res;
+   
    int fd, len, i;
    int band_id, group_id, bit_rate, ioctl_err;
-   char* devname, buf[128];
-   char buffer[80];
+   char *devname;
+   char buf[128];
+	char buf2[40];
+   char buf3[40];
    unsigned long pkt_cnt;
+	unsigned long time_value=0;
    time_t tt;
+   
+   char * url = "http://rss.wunderground.com/auto/rss_full/global/stations/10575.xml?units=metric";
      
    /** values sensor 2 */
    int8_t Sensor2Value=0;
@@ -80,10 +83,12 @@ int main(int argc, char** argv)
    int8_t Sensor3Value=0;
    int8_t Sensor3Correction=-50;
    
+   float Sensor4Value;
+   
    int8_t fs;
    char *filename2 = "/home/pi/sensorvalue2";
    char *filename3 = "/home/pi/sensorvalue3";
-   mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+   FILE * file;
    
    devname = RF12_TESTS_DEV;
    
@@ -104,7 +109,7 @@ int main(int argc, char** argv)
      // this demonstrates how to use ioctl() to read and write config data
    ioctl_err = 0;
    group_id=212;
-   ioctl_err |= ioctl(fd, RFM12B_IOCTL_SET_GROUP_ID, &group_id);
+   //ioctl_err |= ioctl(fd, RFM12B_IOCTL_SET_GROUP_ID, &group_id);
    ioctl_err |= ioctl(fd, RFM12B_IOCTL_GET_GROUP_ID, &group_id);
    ioctl_err |= ioctl(fd, RFM12B_IOCTL_GET_BAND_ID, &band_id);
    ioctl_err |= ioctl(fd, RFM12B_IOCTL_GET_BIT_RATE, &bit_rate);
@@ -123,7 +128,7 @@ int main(int argc, char** argv)
    
    printf("RFM12B configured to GROUP %i, BAND %i, BITRATE: 0x%.2x.\n\n",
       group_id, band_id, bit_rate);
-   
+
    pkt_cnt = 0;
    running = 1;
    do {      
@@ -131,12 +136,49 @@ int main(int argc, char** argv)
       
       time (&tt);
       
+      if (time(0)>time_value+600)     // save data with interval of 10 minutes
+       {
+       curl = curl_easy_init();
+       if(curl)
+       {
+         curl_easy_setopt(curl, CURLOPT_URL, url);
+         curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+       
+         res = curl_easy_perform(curl); // get weather data from rss feed, use of write_data routine
+       
+         curl_easy_cleanup(curl);
+        }
+        
+        file = fopen("/sys/bus/w1/devices/28-000002878f35/w1_slave","r");
+        if (file == NULL) perror ("Error opening file");
+        else
+        {
+        fgets (buf3, 100, file);
+        fgets (buf3, 100, file);
+        fclose (file);
+        Sensor4Value = atof(&buf3[29])/1000;
+        //printf("%.1f\n", Sensor4Value);
+        }
+        
+        file = fopen("/var/www/data.json","r+");
+        if (file == NULL) perror ("Error opening file");
+        else
+        {
+        time_value = time(0);  
+   	  sprintf(buf2, "%d%s", time_value,"000");
+        //printf("%s\n",buf2);
+        
+		  fseek(file, -1, SEEK_END);
+        fprintf(file, ",\n[%s,%d,%d,%d,%.1f,%.1f]]", buf2, Sensor2Value, humidity, Sensor3Value, Sensor4Value, temperature);
+        fclose(file);
+        }
+        }
+      
       if (len > 0) {
-         /*printf("%s", ctime(&tt));
+        /* printf("%s", ctime(&tt));
          printf("\t%i bytes read\n\t\t", len);
   
-         //printf("%d\n", time(0));    // print utc time
-         
          for (i=0; i<len; i++) {
             printf("%d ", buf[i]);
          }
@@ -144,35 +186,39 @@ int main(int argc, char** argv)
          if (buf[0]=='A')					    // Sensor A
 			{
             Sensor2Value = atoi(&buf[1]);		 // convert in integer
-   			if (Sensor2Value != 0)
-                     Sensor2Value += Sensor2Correction;  // correct value if conversion ok
+			if (Sensor2Value != 0)
+    	 		Sensor2Value += Sensor2Correction;  // correct value if conversion ok
    
-           // printf("Sensorvalue 2: %d \n", Sensor2Value);
+            //printf("Sensorvalue 2: %d \n", Sensor2Value);
             
-            FILE * file = fopen( filename2, "w" );
-            fprintf (file, "%d",Sensor2Value);
-   			fclose( file );
+          //  FILE * file = fopen( filename2, "w" );
+          //  fprintf (file, "%d",Sensor2Value);
+   		 //  fclose( file );
          }
          
-          if (buf[0]=='B')					    // Sensor B
+          if (buf[0]=='C')					    // Sensor B
 			{
             Sensor3Value = atoi(&buf[1]);		 // convert in integer
    			if (Sensor3Value != 0)
                      Sensor3Value += Sensor3Correction;  // correct value if conversion ok
    
-           // printf("Sensorvalue 2: %d \n", Sensor3Value);
+            //printf("Sensorvalue 3: %d \n", Sensor3Value);
             
-            FILE * file = fopen( filename3, "w" );
-            fprintf (file, "%d",Sensor3Value);
-   			fclose( file );
+          //  FILE * file = fopen( filename3, "w" );
+          //  fprintf (file, "%d",Sensor3Value); 
+   		 //  fclose( file );
          }
-         fflush(stdout);   
-   
-         pkt_cnt++;
-      } else if (len < 0) {
-         break;
-      }      
-   } while (running);
+        
+        fflush(stdout);
+        pkt_cnt++;
+      }
+      else if (len < 0)
+      {
+        break;
+      }
+      
+    } 
+    while (running);
    
    print_stats(fd);
    
